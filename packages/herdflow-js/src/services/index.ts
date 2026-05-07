09859d71905fb5df9321b604a2024983dfdfd01c
yakush <its.yak@gmail.com>
2026-05-07 04:44:53 +0300

    moved

--- packages/herdflow-js/src/services/index.ts
+++ packages/herdflow-js/src/services/index.ts
@@ -1,4 +1,5 @@
 export * from './helpers.js';
+export * from './rawServiceFactory.js';
 export * from './service.js';
 export * from './serviceFactory.js';
 export * from './types/index.js';
--- packages/herdflow-js/src/services/serviceFactory.ts
+++ packages/herdflow-js/src/services/serviceFactory.ts
@@ -1,71 +1,8 @@
 import type { EMPTY } from '../core/types.js';
-import type { InferState } from '../state/index.js';
-import type { RawStateProvider } from '../state/rawStateProvider.js';
-import { RawService_imp } from './internal/rawService_imp.js';
 import { Service_imp } from './internal/service_imp.js';
-import type { RawService, RawServiceParams } from './rawService.js';
 import type { Service, ServiceParams } from './service.js';
 import type { ServiceDescriptor } from './types/types.js';
 
-//-------------------------------------------------------
-//-- RAW
-//-------------------------------------------------------
-
-type RawFactoryParams<SProvider extends RawStateProvider<any>> = {
-  stateProvider: SProvider;
-  options?: RawServiceParams;
-};
-
-//-------------------------------------------------------
-
-// implicit ServiceDescriptor {state:...} + no name
-export function createRawService<SProvider extends RawStateProvider<any>>(
-  params: RawFactoryParams<SProvider>,
-): RawService<{ state: InferState<SProvider> }, SProvider>;
-
-// implicit ServiceDescriptor {state:...} + name
-export function createRawService<SProvider extends RawStateProvider<any>>(
-  name: string,
-  params: RawFactoryParams<SProvider>,
-): RawService<{ state: InferState<SProvider> }, SProvider>;
-
-// explicit ServiceDescriptor + no name
-export function createRawService<
-  D extends ServiceDescriptor,
-  SProvider extends RawStateProvider<any>,
->(params: RawFactoryParams<SProvider>): RawService<D, SProvider>;
-
-// explicit ServiceDescriptor + name
-export function createRawService<
-  D extends ServiceDescriptor,
-  SProvider extends RawStateProvider<any>,
->(name: string, params: RawFactoryParams<SProvider>): RawService<D, SProvider>;
-
-//implementation:
-
-export function createRawService<
-  D extends ServiceDescriptor,
-  SProvider extends RawStateProvider<any>,
->(...args: unknown[]): RawService<D, SProvider> {
-  let name: string;
-  let params: RawFactoryParams<SProvider>;
-
-  if (typeof args[0] === 'string') {
-    name = args[0];
-    params = args[1] as RawFactoryParams<SProvider>;
-  } else {
-    name = 'untitled';
-    params = args[0] as RawFactoryParams<SProvider>;
-  }
-
-  const { options, stateProvider } = params;
-  return new RawService_imp(name, stateProvider, options);
-}
-
-//SERVICE
-
-//-------------------------------------------------------
-//-- Service
 //-------------------------------------------------------
 
 type ServiceFactoryParams<S> = {
